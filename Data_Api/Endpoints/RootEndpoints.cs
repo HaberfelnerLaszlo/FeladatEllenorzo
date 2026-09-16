cf6b230aa80444d3d49fc7ee3bc2218bc9c78de1
HaberfelnerLaszlo <haberfelner@peterfysuli.hu>
2026-09-16 20:14:06 +0200

    most OK?

--- Data_Api/Endpoints/RootEndpoints.cs
+++ Data_Api/Endpoints/RootEndpoints.cs
@@ -7,11 +7,7 @@ namespace Data_Api.Endpoints
     {
         public static void AddEndpoints(this WebApplication app)
         {
-<<<<<<< Updated upstream
-            app.MapGet("/", () => { return "version:6.6.3 2026.01.17"; });
-=======
             app.MapGet("/", () => { return "version:7.1.1 2026.09.11"; });
->>>>>>> Stashed changes
         }
     }
 }
